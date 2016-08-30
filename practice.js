2b9109ac0f68a7940b103aa9bd8dc1ae5e70278a
asemle <asemle@umass.edu>
2016-08-30 14:18:16 -0500

    first push

--- practice.js
+++ practice.js
@@ -1,19 +1,21 @@
 //We're in a job interview. Answer the following questions (try to not look at your notes unless you have to).
   // 1) What is the purpose of the 'this keyword'?
 
-      //Answer
+      //to set the scope of a property or function to the parent object,
 
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
 
-      //Answer
+      //1. By default, 'this' refers to the global object, or the current window
+      //2. 'this' in the context of a function which is a property of an object, refers to the scope of the object
+      //3. 'this' in the context of a constructor function will refer to the newly created object once the constructor function is invoked using the 'new' keyword
+      //4. When a function is called using call or apply, this refers to the first argument passed to call or apply. If the first argument is null or not an object, this refers to the global object.
 
   // 3) What is the difference between call and apply?
-
-      //Answer
+      // call is used when you want to set an object as the scope of a function, and pass a few arguments to the function, you can use apply when you want to set the scope and also pass an array to the function, which it unpacks using each value at each index as an argument.
 
   // 4) What does .bind do?
 
-      //Answer
+      //.bind pairs a function with an object, so that no matter how the function is called, the 'this' keyword refers to the object
 
 
 //Next Problem
@@ -23,15 +25,31 @@
   //email --> which is a string
   //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
-    //Code Here
+var user = {
+  username: "me",
+  email: "me@gmail.com",
+  getUsername: function() {
+    return this.username;
+  }
+}
 
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
-
+user.getUsername();
 
 //Next Problem
 
 
-// Write the function definitions which will make the following function invocations function properly.
+function Car(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.move = 0;
+    this.moveCar = function() {
+      this.move += 10;
+      return this.move;
+    }
+
+}
 
   //Function Invocations Here
 
